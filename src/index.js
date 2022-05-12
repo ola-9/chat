60fa4d96f95e1504214e364b6c7c4c6fc16d014e
ola-9 <nepogodda@gmail.com>
2022-05-12 16:37:03 +0300

    move root inside index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { io } from 'socket.io-client';
 import '../assets/application.scss';
 import init from './init.jsx';
 
+const root = ReactDOM.createRoot(document.getElementById('chat'));
+
 const runApp = () => {
   const socket = io();
   const test = init(socket);
-  const root = ReactDOM.createRoot(document.getElementById('chat'));
+  // const root = ReactDOM.createRoot(document.getElementById('chat'));
   root.render(
     test,
   );
