52f8df17d9b8b1ef56d9a5a664ac024b3bb9b4d7
ola-9 <nepogodda@gmail.com>
2022-05-12 16:41:24 +0300

    fix root in index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import { io } from 'socket.io-client';
 import '../assets/application.scss';
 import init from './init.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('chat'));
+// const root = ReactDOM.createRoot(document.getElementById('chat'));
 
 const runApp = () => {
   const socket = io();
   const test = init(socket);
   // const root = ReactDOM.createRoot(document.getElementById('chat'));
-  root.render(
-    test,
-  );
+  ReactDOM.createRoot(document.getElementById('chat')).render(test);
 
   // return test;
 };
