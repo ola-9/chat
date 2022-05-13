936ed3adc1a5aad0de64ebfa227fbbf287054ea2
ola-9 <nepogodda@gmail.com>
2022-05-13 18:13:18 +0300

    complete NotFound Component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import notFoundImage from '../../assets/404.jpg';
 
-function NotFound() {
-  return (
-    <h1>NotFound</h1>
-  );
-}
+const NotFound = () => (
+  <Container className="text-center">
+    <img
+      alt="страница не найдена"
+      className="img-fluid"
+      src={notFoundImage}
+      style={{ maxWidth: '100%', height: 'auto' }}
+    />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти на &nbsp;
+      <a href="/">на главную страницу</a>
+    </p>
+  </Container>
+);
 
 export default NotFound;
